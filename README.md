9ec724abffaf67da16148635703e8589995d0e18
rbetik12 <mr.prikota@gmail.com>
2019-01-14 20:41:06 +0300

    Wrote rounding function. Small background changes. Updated README

--- README.md
+++ README.md
@@ -3,17 +3,18 @@
 Calculator for an android(SDK 27-28)
 
 **TO DO**:
-- Make round algorithm
-- Make output match editText scale (auto font size changing)
+- ~~Make round algorithm~~
+- ~~Make output match editText scale (auto font size changing)~~ Make output shift automatically
 - Add branches buttons
 - Add pow and sqrt functions
 - ~~Add clear and CE buttons~~
 - ~~Add point button~~
 - Add equation exceptions catcher (recent doesn't work)
 - Make a pretty GUI
+- Add cos/sin/etc. functions
 
 
 
 
-*(Long plans)*
+*(Long-term plans)*
 - Add graphing
--- app/src/main/java/io/github/rbetik12/calculator/MainActivity.kt
+++ app/src/main/java/io/github/rbetik12/calculator/MainActivity.kt
@@ -6,6 +6,9 @@ import android.util.Log
 import android.view.View
 import net.objecthunter.exp4j.*
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Math.abs
+import java.lang.Math.pow
+import kotlin.math.round
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +16,8 @@ class MainActivity : AppCompatActivity() {
     private var equation: String = " "
     private var result: Double = 0.0
     private lateinit var expression: Expression
+    private val MAX_OUTPUT_LENGTH: Int = 19
+    private val MAX_SYMBOLS_IN_DOUBLE_WEXP: Int = 7
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -103,27 +108,58 @@ class MainActivity : AppCompatActivity() {
                     return
                 }
                 result = expression.evaluate()
-                val resultToStr = result.toString()
-                Log.d("OUTPUT", "Substring is ${resultToStr.substring(resultToStr.length - 2, resultToStr.length)}")
-                if (resultToStr.substring(resultToStr.length - 2, resultToStr.length) == ".0"){
-                    resultEquation.setText(result.toInt().toString())
-                    Log.d("OUTPUT", "Result is $result now if")
-                }
-                else {
+                val isInt: Boolean = checkForInt(result)
+                if (isInt){
                     resultEquation.setText(result.toString())
-                    Log.d("OUTPUT", "Result is $result now")
+                    return
                 }
+                setOutput(result)
             }
             R.id.buttonClearEverything -> {
                 equation = ""
                 result = 0.0
-                resultEquation.setText(null)
+                resultEquation.text = null
             }
             R.id.buttonClear -> {
                 equation = equation.substring(0, equation.length - 1)
                 resultEquation.setText(equation)
             }
         }
+    }
+    private fun checkForInt(result: Double): Boolean{
+        /**
+         * Checks if result can be put in output as integer(e.g. 12.0 -> 12)
+         * @param [result] Result of equation
+         * @return [true] if Int [false] if not
+         */
+        val resultToStr = result.toString()
+
+        if (resultToStr.substring(resultToStr.length - 2, resultToStr.length) == ".0")
+            return true
+        return false
+    }
+
+    private fun setOutput(result: Double){
+        /**
+         * Rounds double result, if it contains lots of digits with exponent
+         * @param result
+         * @return nothing
+         */
+        val resultToStr: String = result.toString()
+
+        Log.d("OUTPUT","Result is $result")
+        Log.d("OUTPUT", "ResultStr is $resultToStr")
+        if (resultToStr.length > MAX_OUTPUT_LENGTH && resultToStr.indexOf("E") != -1){
+            Log.d("OUTPUT", "lmao")
+            val exp: Double = abs(resultToStr.substring(resultToStr.indexOf("E") + 1, resultToStr.lastIndex + 1).toDouble())
+            Log.d("OUTPUT","Exp is $exp")
+            var tempResult: Double = result
+            tempResult = Math.round(tempResult * pow(10.0, exp + MAX_SYMBOLS_IN_DOUBLE_WEXP)) / pow(10.0, exp + MAX_SYMBOLS_IN_DOUBLE_WEXP)
+            Log.d("OUTPUT","Tempresult is $tempResult")
+            resultEquation.setText(tempResult.toString())
+        }
+        else
+            resultEquation.setText(resultToStr)
     }
 }
 
